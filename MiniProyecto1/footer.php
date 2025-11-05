cbcaa7aebd103aeef4168516ffd8a649889ad402
Britney Valoy <132220067+britval@users.noreply.github.com>
2025-11-05 15:47:26 -0500

    Submódulo MiniProyecto1 eliminado correctamente

--- MiniProyecto1/footer.php
+++
@@ -1,5 +0,0 @@
-<footer style="margin-top: 50px; text-align:center;">
-    <hr>
-    <p>Universidad Tecnológica de Panamá - Ingeniería Web<br>
-    Fecha de ejecución: <?php echo date("d/m/Y"); ?></p>
-</footer>
--- MiniProyecto1/utils/Navigation.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Navigation {
-    public static function backToMenu($url = "../index.php") {
-        echo "<br><a href='{$url}' style='color:blue;'>Volver al menú principal</a>";
-    }
-}
-?>
